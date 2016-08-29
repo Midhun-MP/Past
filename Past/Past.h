14ae138e35a47cab92ce21959aa29854488cf19a
Midhun MP <midhunmp7@gmail.com>
2016-08-29 13:17:42 +0530

    Added project to repo

---
+++ Past/Past.h
@@ -0,0 +1,20 @@
+//
+//  Past.h
+//  Past
+//
+//  Created by Midhun on 29/08/16.
+//  Copyright © 2016 Midhun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Past-Swift.h"
+
+//! Project version number for Past.
+FOUNDATION_EXPORT double PastVersionNumber;
+
+//! Project version string for Past.
+FOUNDATION_EXPORT const unsigned char PastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Past/PublicHeader.h>
+
+
